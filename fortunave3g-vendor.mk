92a9a5ecc56b45bd9972a08fe9d6ea07abd24046
ZIM <zim555@mail.ru>
2016-11-17 21:15:30 +0200

    Block non fortunave3g

--- fortunave3g-vendor.mk
+++ fortunave3g-vendor.mk
@@ -1,5 +1,10 @@
+SAMSUNG_TARGETS := fortunave3g
+ifneq ($(filter $(SAMSUNG_TARGETS),$(TARGET_DEVICE)),)
+
 # Pick up overlay for features that depend on non-open-source files
 DEVICE_PACKAGE_OVERLAYS := vendor/samsung/fortunave3g/overlay
 
 # Include non-open-source files
 $(call inherit-product, vendor/samsung/fortunave3g/fortunave3g-vendor-blobs.mk)
+
+endif
